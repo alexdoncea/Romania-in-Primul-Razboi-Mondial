42a0ee5a934983a9b4c8a746eee7df09fe6b35c6
AlexDoncea <34485926+alexdoncea@users.noreply.github.com>
2018-05-25 04:22:13 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Valcea_In_Primul_Razboi_Mondial
+{
+    partial class ValceaInPrimulRazboiMondial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ValceaInPrimulRazboiMondial));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRamnicuValcea = new System.Windows.Forms.Button();
+            this.btnBrezoi = new System.Windows.Forms.Button();
+            this.btnCiunget = new System.Windows.Forms.Button();
+            this.btnMadulari = new System.Windows.Forms.Button();
+            this.iTalk_TabControl1 = new iTalk.iTalk_TabControl();
+            this.iTalk_TabControl1.SuspendLayout();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            //
+            //iTalk_TabControl1
+            //
+            this.iTalk_TabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.iTalk_TabControl1.Controls.Add(this.tabPage1);
+            //
+            //tabPage1
+            //
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            //this.tabPage1.BackgroundImage = global::Valcea_In_Primul_Razboi_Mondial.Properties.Resources.master242_image003;
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.tabPage1.Location = new System.Drawing.Point(600, 300);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1013, 670);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Valcea";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(709, 888);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRamnicuValcea
+            // 
+            this.btnRamnicuValcea.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRamnicuValcea.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRamnicuValcea.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnRamnicuValcea.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnRamnicuValcea.Location = new System.Drawing.Point(559, 383);
+            this.btnRamnicuValcea.Name = "btnRamnicuValcea";
+            this.btnRamnicuValcea.Size = new System.Drawing.Size(30, 30);
+            this.btnRamnicuValcea.TabIndex = 1;
+            this.btnRamnicuValcea.UseVisualStyleBackColor = false;
+            this.btnRamnicuValcea.Click += new System.EventHandler(this.btnRamnicuValcea_Click);
+            //
+            // btnBrezoi
+            //
+            this.btnBrezoi.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBrezoi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrezoi.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnBrezoi.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnBrezoi.Location = new System.Drawing.Point(482, 185);
+            this.btnBrezoi.Name = "btnBrezoi";
+            this.btnBrezoi.Size = new System.Drawing.Size(30, 30);
+            this.btnBrezoi.TabIndex = 1;
+            this.btnBrezoi.UseVisualStyleBackColor = false;
+            this.btnBrezoi.Click += new System.EventHandler(this.btnBrezoi_Click);
+            //
+            // btnCiunget
+            //
+            this.btnCiunget.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCiunget.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCiunget.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCiunget.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnCiunget.Location = new System.Drawing.Point(280, 162);
+            this.btnCiunget.Name = "btnCiunget";
+            this.btnCiunget.Size = new System.Drawing.Size(30, 30);
+            this.btnCiunget.TabIndex = 1;
+            this.btnCiunget.UseVisualStyleBackColor = false;
+            this.btnCiunget.Click += new System.EventHandler(this.btnCiunget_Click);
+            //
+            //btnMadulari
+            //
+            this.btnMadulari.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMadulari.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMadulari.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnMadulari.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnMadulari.Location = new System.Drawing.Point(360, 714);
+            this.btnMadulari.Name = "btnMadulari";
+            this.btnMadulari.Size = new System.Drawing.Size(30, 30);
+            this.btnMadulari.TabIndex = 1;
+            this.btnMadulari.UseVisualStyleBackColor = false;
+            this.btnMadulari.Click += new System.EventHandler(this.btnMadulari_Click);
+            // 
+            // ValceaInPrimulRazboiMondial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(670, 933);
+            this.Controls.Add(this.btnRamnicuValcea);
+            this.Controls.Add(this.btnBrezoi);
+            this.Controls.Add(this.btnCiunget);
+            this.Controls.Add(this.btnMadulari);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ValceaInPrimulRazboiMondial";
+            this.Text = "Valcea In Primul Razboi Mondial";
+            //this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRamnicuValcea;
+        private System.Windows.Forms.Button btnBrezoi;
+        private System.Windows.Forms.Button btnCiunget;
+        private System.Windows.Forms.Button btnMadulari;
+        private iTalk.iTalk_TabControl iTalk_TabControl1;
+        private iTalk.iTalk_ThemeContainer iTalk_ThemeContainer1;
+        private System.Windows.Forms.TabPage tabPage1;
+        //private readonly EventHandler Form1_Load;
+        //private EventHandler Form1_Load;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Valcea_In_Primul_Razboi_Mondial
+{
+    public partial class ValceaInPrimulRazboiMondial : Form
+    {
+        public ValceaInPrimulRazboiMondial()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRamnicuValcea_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Aici se afla Regimentele 2 si 42 Infanterie Valcea, la baza militara din Goranu",
+            "Ramnicu Valcea");
+        }
+
+        private void btnBrezoi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("",
+            "Brezoi");
+        }
+
+        private void btnCiunget_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Puţină lume ştie, mai mult eco-turiştii, bicicliştii, că Transalpina mai are o soră. Un alt drum de poveste, la 2000 de metri, un drum construit de nemţi şi care traversează Munţii Latoriţei, unind Transalpina de DN7a, în Mălaia, traversând vârfuri care au în medie 2000 de metri. Poartă un nume de cod „Strategica”, dar este de departe un drum care nu are niciun concurent, este drumul care merge doar prin nori şi care este seva naturii, drumul muntelui, drumul de basm, drumul care dă dependenţă, este drogul Vâlcii." +
+                "Nu toate drumurile sunt la fel. Sunt drumuri de asfalt, care doar duc de la un loc la altul, și sunt drumuri cărări. Diferența dintre un drum și o cărare nu este una atât de evidentă. O cărare e ceva mai mult decât o obișnuință care vine cu cunoașterea unui loc. E este o formă de contact cu un anumit peisaj. Ea nu este distructivă.Este o adaptare perfectă, prin experiență și familiaritate, a mișcării printr - un anumit loc. Ea respectă contururile naturale, cum ar fi obstacolele naturale pe care le întâlnește și le ocolește. Mergând pe o cărare, doar te integrezi în peisajul prin care te miști, fără a-l afecta în vreun fel. Strategica este un astfel de drum." +
+                "Pornind din Transalpina, din apropiere de șoseaua Ștefanu și mergând cale de aproape 50 de kilometri pe creasta munților Latoriței, Strategica sau Drumul Strategic este o invitație pentru pasionații de mountainbike și nu numai la adevărata aventură. După cum am spus, Strategica sau Drumul Strategic este un vechi drum militar construit de germani (chiar de Erwin Rommel) în timpul Primului Război Mondial și pornește din Drumul Regelui (Transalpina), de la 1915 metri altitudine, și urmează, la câteva zeci de metri mai jos, cu destulă exactitate, creasta munților Latoriței, oferind largi perspective asupra munților ce îi înconjoară: Parâng, Căpățânii, Lotrului, cât și a văilor ce îi separă: Latoriței și Lotrului." +
+                "Lăsăm abrupturile stâncoase ale Parângului de vest în spate şi se porneşte pe Transalpina. Drumul începe cu o porțiune în ușoară coborâre. După ce se trece pe lângă câteva tabere de ciupercari, se pot admira pârtiile de schi de la Transalpina Ski Resort. Imediat  începe panorama lacului Vidra și culmile Lotrului ce se înșiră liniștit de partea cealaltă a văii. De aici, se traversează  muntele Coasta Benghii și începe urcuşul spre vârful Zănoguța. Drumul ajunge în Vârful Puru – 2049 de metri, după care se îndreaptă spre zona cea mai frumoasă a crestei munților Latoriței –  muntele calcaros Boarneșu. Porțiunea între vârful Pietrile și vârful Mogoșu este spectaculoasă. Strategica continuă urcarea spre cel mai înalt vârf din masiv  – Fratoșteanu – 2053 metri, drumul trecând doar la 40 de metri diferență de nivel sub vârf. Urmează acum coborârea de pe creastă, se traversează o zonă împădurită și apoi se ajunge într - o mare poiană, care chiar aşa are numele – Plaiul Poienii. Aici, în dreptul stânei, drumul se bifurcă: în timp ce Drumul Strategic își continuă calea, de aici ceva mai firav, de - a lungul crestei, un al doilea drum pietruit o ia la dreapta, către Valea Rudăreasa. Mult mai practicabilă Strategica este pe Valea Rudăresei, fiind străjuită în stânga de un spectaculos perete calcaros  – Pietrile Albe. Coborârea pe valea Rudăreasa este minunată, combinația între albul calcarelor, strălucirile apei, verdele pădurii și calitatea drumului transformând această coborâre într - o experiență de neuitat.",
+                "Ciunget - Strategica");
+        }
+        private void btnMadulari_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("content",
+                            "Madulari");
+        }
+    }
+}
